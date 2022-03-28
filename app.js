083a53f728209a2e558ab7db95812805b8c4f65a
AD <archied5150@yahoo.com>
2022-03-28 16:04:29 -0500

    create html file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+//import the file system from node
+const fs = require('fs');
+//import the local file that has the webpage data
+const generatePage = require('./src/page-template.js')
+// grabs the user data entered and reduces it to only have the data entered...
 const profiledataArgs = process.argv.slice(2, process.argv.length);
+// ...and assign them to variables
+const [name, github] = profiledataArgs;
 
-const printProfileData = profileDataArr => {
-    //this...
-    for(let i=0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('================');
+//generate the index.html file using the data entered
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    //is the same as...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-}
-
-printProfileData(profiledataArgs);
+    console.log('portfolio complete! check out index.html to see the output!');
+});
