8725d227af6e4d603c46d26eb6fa482ba3d87b07
AD <archied5150@yahoo.com>
2022-03-28 21:40:36 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-//import the file system from node
-const fs = require('fs');
-//import the local file that has the webpage data
-const generatePage = require('./src/page-template.js')
-// grabs the user data entered and reduces it to only have the data entered...
-const profiledataArgs = process.argv.slice(2, process.argv.length);
-// ...and assign them to variables
-const [name, github] = profiledataArgs;
+//import the inquirer module
+const inquirer = require('inquirer');
+// //import the file system from node
+// const fs = require('fs');
+// //import the local file that has the webpage data
+// const generatePage = require('./src/page-template.js')
 
+// //generate the webpage using the obtained data, and assign it to the variable
+// const pageHTML = generatePage(name, github);
 
-//generate the index.html file using the data entered
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// //generate the index.html file using the data entered
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('portfolio complete! check out index.html to see the output!');
-});
+//     console.log('portfolio complete! check out index.html to see the output!');
+// });
+
+//create the question prompt
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    //display the answers
+    .then(answers => console.log(answers));
