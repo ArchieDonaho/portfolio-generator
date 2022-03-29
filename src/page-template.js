f5807ba127f0a3516340914b27d73053b15947b5
AD <archied5150@yahoo.com>
2022-03-29 16:06:15 -0500

    added multiple projects

--- src/page-template.js
+++ src/page-template.js
@@ -1,21 +1,103 @@
-// generate html when the function is called
-const generatePage = (name, github) => {
+//create the about section
+const generateAbout = aboutText => {
+    //if there is no about section, return nothing
+    if(!aboutText){
+        return '';
+    }
+    //if there is an about section, return html to display it
     return `
-        <!DOCTYPE html> 
-        <html lang="en"> 
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>Portfolio Demo</title>
-            </head>
-        
-            <body>
-                <h1>${name}</h1>
-                <h2><a href="https://github.com/${github}">Github</a></h2>
-            </body>
-        </html>
+    <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+    </section>
     `;
 };
 
-module.exports = generatePage;
+//generate the project's html based on it's feature status
+const generateProjects = projectsArr => {
+    return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+            .filter(({ feature }) => feature) //checks feature status
+            .map(({ name, description, languages, link }) => {
+                //if the project is featured, generate the following html and add it
+                return `
+                    <div class="col-12 mb-2 bg-dark text-light p-3">
+                    <h3 class="portfolio-item-title text-light">${name}</h3>
+                    <h5 class="portfolio-languages">
+                        Built With:
+                        ${languages.join(', ')}
+                    </h5>
+                    <p>${description}</p>
+                    <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                    </div>
+                `;
+            })
+            .join('')}
+  
+        ${projectsArr
+            .filter(({ feature }) => !feature) //checks feature status
+            .map(({ name, description, languages, link }) => {
+                //if the project is not featued, generate the following html and add it
+                return `
+                    <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                    <h3 class="portfolio-item-title text-light">${name}</h3>
+                    <h5 class="portfolio-languages">
+                        Built With:
+                        ${languages.join(', ')}
+                    </h5>
+                    <p>${description}</p>
+                    <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                    </div>
+                `;
+            })
+            .join('')}
+
+        </div>
+      </section>
+    `;
+};
+
+//bring in the promise object returned by inquirer & export the html
+module.exports = templateData => {
+    //destructure projects and baout data from templateData based on their property key names
+    const {projects, about, ...header} = templateData;
+    // projects = templateData.projects
+    // about = templateData.about
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+  
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+    ${generateAbout(about)}
+    ${generateProjects(projects)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+};
